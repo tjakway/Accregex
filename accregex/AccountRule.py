21f0299d277c5024b249dd6a838a24edbeff1a82
Thomas Jakway <tjakway@nyu.edu>
2016-05-25 17:11:45 -0700

    added source account field to AccountRule

--- accregex/AccountRule.py
+++ accregex/AccountRule.py
@@ -2,20 +2,34 @@ import json
 from .eprint import eprint
 
 class AccountRule:
-    def __init__(self, rule_name, regex_str, priority, dest):
+    def __init__(self, rule_name, regex_str, priority, dest, src):
        self.rule_name = rule_name
        self.priority = priority
        self.regex = re.compile(regex_str)
        #the destination account
        self.dest = dest
+       self.src = src
 
 #construct an AccountRule object from a 2nd-tier JSON object
-def _json_obj_to_account_rule(rule_name, obj):
+def _json_obj_to_account_rule(rule_name, global_src_account=None, obj):
+    #either use the global source account or the source account specified for this rule
+    #prefer the per-rule source account if both exist
+    #if neither exist it's an error
     try:
+        if global_src_account is None:
+            try:
+                src_acc = obj['src']
+            except:
+                eprint("Must specify either a default (global) source account with a top-level 'src' element or a per-rule source account.")
+                raise
+        else:
+            src_acc = global_src_account
+
         return AccountRule(rule_name, \
                 obj['regex'], \
                 obj['priority'], \
-                obj['dest'])
+                obj['dest'], \
+                src_acc)
     except re.error:
         eprint("error compiling regex expression in AccountRule from json object {}".format(str(obj)))
         raise
@@ -26,13 +40,20 @@ def read_account_rules(json_file):
     json_data = json.load(json_file)
     all_rules = []
 
+    #can optionally specify a "src" element as the default source account
+    #can be overridden on a per-rule basis
+    if "src" in json_data.keys():
+        default_src = json_data["src"]
+    else:
+        default_src = None
+
     #read in each account rule from each JSON sub-object 
     #the top-level keys correspond to each rule
     for key in json_data.keys():
         rule_name = key
         
         try:
-            all_rules.append(_json_obj_to_account_rule(rule_name, json_data[rule_name]))
+            all_rules.append(_json_obj_to_account_rule(rule_name, default_src, json_data[rule_name]))
         except:
             eprint("Could not create a rule from JSON object {}".format(rule_name))
             raise
