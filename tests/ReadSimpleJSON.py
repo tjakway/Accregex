ecc12fcaeb4631431bcf34a7db981852ee0119ba
Thomas Jakway <tjakway@nyu.edu>
2016-05-25 17:11:24 -0700

    simple unit test and input json

---
+++ tests/ReadSimpleJSON.py
@@ -0,0 +1,11 @@
+import unittest
+#need to mess with PYTHONPATH to import from a relative location
+import PathDirs
+PathDirs.add_parent_to_path()
+import AccountRule
+
+
+
+class ReadSimpleJSONTests(unittest.TestCase):
+    def test_simple_json(self):
+        self.assertEqual(len(AccountRule.read_account_rules("match_all_unspecified.json")), 1)
