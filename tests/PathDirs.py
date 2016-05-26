e7457876adeef9a7713b9941f99ea92a9cd13f6e
Thomas Jakway <tjakway@nyu.edu>
2016-05-25 17:05:58 -0700

    working on getting dynamic import working

--- tests/PathDirs.py
+++ tests/PathDirs.py
@@ -6,15 +6,19 @@ def add_parent_to_path():
     #get the parent directory of wherever this script is located
     #(i.e. the script location, not the working directory)
     #see http://stackoverflow.com/questions/30218802/get-parent-of-current-directory-from-python-script
-    parent_dir = os.path.dirname(sys.path[0])
-    if parent_dir == '' or parent_dir is None:
-        parent_dir = os.getcwd()
+#    parent_dir = os.path.abspath(os.path.dirname(sys.path[0]))
+#    if parent_dir == '' or parent_dir is None:
+#        parent_dir = os.getcwd()
+
+    parent_dir = os.path.abspath("..")
 
     #add the parent directory to PYTHONPATH
     #see http://stackoverflow.com/questions/4934806/how-can-i-find-scripts-directory-with-python
     sys.path.insert(0, parent_dir)
 
     #add all of its child directories to PYTHONPATH
-    for f in os.listdir(parent_dir):
+    for f in os.join(parent_dir, os.listdir(parent_dir)):
         if os.path.isdir(f):
             sys.path.insert(0, f)
+
+    print sys.path
