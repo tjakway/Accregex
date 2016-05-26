eac67b82a8dfc458e90739292c7eabb038602ace
Thomas Jakway <tjakway@nyu.edu>
2016-05-25 17:20:39 -0700

    now getting absolute path via ..

--- tests/PathDirs.py
+++ tests/PathDirs.py
@@ -3,12 +3,7 @@ import os
 
 #add the parent directory of this script's location to PYTHONPATH (*NOT* the parent dir of the current working directory!)
 def add_parent_to_path():
-    #get the parent directory of wherever this script is located
-    #(i.e. the script location, not the working directory)
-    #see http://stackoverflow.com/questions/30218802/get-parent-of-current-directory-from-python-script
-    parent_dir = os.path.abspath(os.path.dirname(sys.path[0]))
-    if parent_dir == '' or parent_dir is None:
-        parent_dir = os.getcwd()
+    parent_dir = os.path.abspath("..")
 
     #add the parent directory to PYTHONPATH
     #see http://stackoverflow.com/questions/4934806/how-can-i-find-scripts-directory-with-python
