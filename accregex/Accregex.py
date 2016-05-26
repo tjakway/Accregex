1acf3ce4bd0e95c07af1356558122f941a5ac55c
Thomas Jakway <tjakway@nyu.edu>
2016-05-25 17:21:57 -0700

    dont need absolute paths since we're importing as a package

--- accregex/Accregex.py
+++ accregex/Accregex.py
@@ -6,8 +6,8 @@ import csv
 import shutil
 import os
 from gnucash import Session, GncNumeric, Split
-import Logger.Logger
-import Args.get_cli_arg_parser
+from .Logger import Logger
+from .Args import get_cli_arg_parser
 
 #create the global logger
 global_logger = Logger()
