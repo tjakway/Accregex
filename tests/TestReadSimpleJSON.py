57ff836db5dd5edbb26e340c69d0216b1b24b5af
Thomas Jakway <tjakway@nyu.edu>
2016-05-29 16:40:36 -0700

    TestReadSimpleJSON now subclasses AccregexTest

--- tests/TestReadSimpleJSON.py
+++ tests/TestReadSimpleJSON.py
@@ -1,10 +1,10 @@
 from AccregexTest import AccregexTest
 
-
-
 class TestReadSimpleJSON(AccregexTest):
+    def setUp(self):
+        AccregexTest.setUp(self)
+
     def test_simple_json(self):
-        
         from accregex import AccountRule
         #list of account rules
         account_rules = AccountRule.read_account_rules("res/match_all_unspecified.json")
