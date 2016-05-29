f50c4d2676d560e858cc844f9ecc6852fe9d0de6
Thomas Jakway <tjakway@nyu.edu>
2016-05-29 16:17:39 -0700

    preventing manipulating PYTHONPATH path

--- tests/PathDirs.py
+++ tests/PathDirs.py
@@ -1,13 +1,18 @@
 import sys
 import os
 
+#check if pypathdir is already in PYTHONPATH and add it if it isn't
+def add_path_no_dup(pypathdir):
+    if not pypathdir in sys.path:
+        sys.path.insert(0, pypathdir)
+
 #add the parent directory of this script's location to PYTHONPATH (*NOT* the parent dir of the current working directory!)
 def add_parent_to_path():
     parent_dir = os.path.abspath("..")
 
     #add the parent directory to PYTHONPATH
     #see http://stackoverflow.com/questions/4934806/how-can-i-find-scripts-directory-with-python
-    sys.path.insert(0, parent_dir)
+    add_path_no_dup(parent_dir)
 
     all_child_dirs = []
     for d in os.listdir(parent_dir):
@@ -19,4 +24,4 @@ def add_parent_to_path():
     #add all of its child directories to PYTHONPATH
     for f in all_child_dirs:
         if os.path.isdir(f):
-            sys.path.insert(0, f)
+            add_path_no_dup(f)
