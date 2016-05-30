0625a86a05e64d2d892a8e643ea82fd82e9f3920
Thomas Jakway <tjakway@nyu.edu>
2016-05-29 17:24:49 -0700

    using fields instead of literal paths

--- tests/TestReadSimpleJSON.py
+++ tests/TestReadSimpleJSON.py
@@ -7,5 +7,5 @@ class TestReadSimpleJSON(AccregexTest):
     def test_simple_json(self):
         from accregex import AccountRule
         #list of account rules
-        account_rules = AccountRule.read_account_rules("res/match_all_unspecified.json")
+        account_rules = AccountRule.read_account_rules(AccregexTest.match_all_unspecified_json)
         self.assertEqual(len(account_rules), 1)
