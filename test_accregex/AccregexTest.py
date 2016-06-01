865107c87ed6c00e9504be6beb42064912ae9b73
Thomas Jakway <tjakway@nyu.edu>
2016-06-01 09:21:59 -0700

    using PathDirs.add_accregex_to_python_path

--- test_accregex/AccregexTest.py
+++ test_accregex/AccregexTest.py
@@ -1,24 +1,17 @@
 import unittest
+from .PathDirs import abs_from_here, add_accregex_to_python_path
 
 """
 The base class for tests
 The purpose of this module is to add the source directory to PYTHONPATH
 """
 
-
-#get the absolute path of a file relative to 
-#see http://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory
-def abs_from_here(path):
-    import os
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
-
 class AccregexTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         import unittest
         #need to mess with PYTHONPATH to import from a relative location
-        import PathDirs
-        PathDirs.add_parent_to_path()
+        add_accregex_to_python_path()
 
     @classmethod
     def tearDownClass(cls):
