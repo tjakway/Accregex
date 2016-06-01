ae10329097c39570cfcea9f612f63043f462f18a
Thomas Jakway <tjakway@nyu.edu>
2016-05-31 18:40:29 -0700

    fixed _log_string

--- accregex/Logger.py
+++ accregex/Logger.py
@@ -2,7 +2,7 @@
 #it just stores a string (thus no side effects)
 class Logger:
     def __init__(self):
-        self.name = ""
+        self._log_string = ""
 
     def write(self, write_str):
         self._log_string += str(write_str)
