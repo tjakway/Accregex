0e60dd216b91d6cb5589ae2e2b56087253f1ba01
Thomas Jakway <tjakway@nyu.edu>
2016-06-01 08:39:04 -0700

    running the package now runs the tests

--- test_accregex/__init__.py
+++ test_accregex/__init__.py
@@ -1,4 +1,5 @@
-import unittest
-
 if __name__ == '__main__':
-    unittest.main()
+    import unittest
+    from .Runner import get_all_test_suites 
+    runner = unittest.TextTestRunner()
+    runner.run(get_all_test_suites())
