fa5db64c4d20aebea63232ed62e7ad4240af07fe
Thomas Jakway <tjakway@nyu.edu>
2018-03-16 13:40:45 -0700

    WIP

--- src/main/scala/com/jakway/gnucash/Accregex.scala
+++ src/main/scala/com/jakway/gnucash/Accregex.scala
@@ -1,7 +1,40 @@
 package com.jakway.gnucash
 
+import scala.xml.{Node, XML}
+
 object Accregex {
   def main(args: Array[String]): Unit = {
-    
+    MatchTrans.test()
+  }
+}
+
+object MatchTrans
+{
+  def test(): Unit = {
+    val loc = "/home/thomas/Downloads/all_finances.xml"
+    val node = XML.load(loc)
+
+    node match {
+      case <gnc:transaction>{values}</gnc:transaction> => {
+        true
+      }
+      case _ => false
+    }
+
+  }
+}
+
+/**
+  * TODO:
+  * -check count-data attribute cd:type=="book" && text=="1"
+  *
+  * -
+  */
+object Validate {
+  private def isBook(node: Node): Boolean = {
+    node.namespace == "gnc" && node.label == "book"
+  }
+  def apply(root: Node) = {
+
   }
 }
