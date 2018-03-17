e42e90ba04f1c4e3d804b303ce9b266513b3d421
Thomas Jakway <tjakway@nyu.edu>
2018-03-17 12:44:54 -0700

    wrote onlyOne

--- src/main/scala/com/jakway/gnucash/Accregex.scala
+++ src/main/scala/com/jakway/gnucash/Accregex.scala
@@ -24,6 +24,8 @@ object MatchTrans
   }
 }
 
+trait ValidationError
+
 /**
   * TODO:
   * -check count-data attribute cd:type=="book" && text=="1"
@@ -31,6 +33,18 @@ object MatchTrans
   * -
   */
 object Validate {
+  private def onlyOne[A](errorType: String => ValidationError)(s: Seq[A]): Either[ValidationError, A] = {
+    val msg = "expected only 1 item in seq but got: "
+    if(s.length == 1) {
+      Right(s.head)
+    } else if(s.isEmpty) {
+      Left(errorType(msg + "empty seq"))
+    }
+    else {
+      Left(errorType(msg + s.toString))
+    }
+  }
+
   private def isBook(node: Node): Boolean = {
     node.namespace == "gnc" && node.label == "book"
   }
