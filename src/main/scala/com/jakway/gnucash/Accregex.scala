9d3c769c53d1e64e7ded77d2c459abe4a98fa02d
Thomas Jakway <tjakway@nyu.edu>
2018-03-18 15:34:28 -0700

    wrote hasAttribute

--- src/main/scala/com/jakway/gnucash/Accregex.scala
+++ src/main/scala/com/jakway/gnucash/Accregex.scala
@@ -1,7 +1,8 @@
 package com.jakway.gnucash
 
 
-import scala.xml.{Node, NodeSeq, XML}
+import scala.util.{Failure, Success, Try}
+import scala.xml.{Attribute, Node, NodeSeq, XML}
 
 object Accregex {
   def main(args: Array[String]): Unit = {
@@ -27,11 +28,17 @@ object MatchTrans
 
 trait ValidationError
 
-/**
-  * boolean functions to run on XML nodes
-  *
-  */
+object ValidationError {
+  def getMsg(t: Throwable): String = {
+    //TODO: get stack trace & msg
+    ???
+  }
+}
+
 object NodeTests {
+  import ValidationError._
+
+  val namespaceSeparator = ":"
 
   def onlyOne[A](s: Seq[A])(implicit errorType: String => ValidationError): Either[ValidationError, A] = {
     val msg = "expected only 1 item in seq but got: "
@@ -65,8 +72,64 @@ object NodeTests {
     }
   }
 
+  /**
+    * the : is used as the separator in XML namespaces
+    * @param s
+    * @param sep
+    * @param errorType
+    * @return
+    */
+  private def splitStringOnLastIndexOf(s: String, sep: String = namespaceSeparator)
+                                      (implicit errorType: String => ValidationError):
+    Either[ValidationError, (String, String)] = {
 
+    val splitIndex = s.lastIndexOf(sep)
+    val prefix = s.substring(0, splitIndex)
+    if(splitIndex >= (s.length - 1)) {
+      Left(errorType("You cannot search for an attribute with a namespace and no name"))
+    } else {
+      val suffix = s.substring(splitIndex + 1, s.length - 1)
+      Right((prefix, suffix))
+    }
+  }
 
+  def hasAttribute(root: Node, name: String)
+                        (implicit errorType: String => ValidationError): Either[ValidationError, Attribute] = {
+
+    val noAttrError = Left(errorType(s"$root does not contain attribute $name"))
+
+    def convResult(x: Option[Seq[Node]]): Either[ValidationError, Attribute] = x match {
+      case None => noAttrError
+      case Some(xs) if xs.isEmpty => noAttrError
+      case Some(xs) => for {
+        r <- onlyOne(xs)
+
+        //convert the possible ClassCastException into an Either
+        asAttr <- Try(r.asInstanceOf[Attribute]) match {
+          case Success(q) => Right(q)
+          case Failure(q) => Left(errorType(getMsg((q))))
+        }
+      } yield {
+        asAttr
+      }
+    }
+
+    //check if the attribute is prefixed
+    val searchRes = name.contains(namespaceSeparator) match {
+      case true => {
+        for {
+          (ns, attrName) <- splitStringOnLastIndexOf(name)
+        } yield {
+          root.attribute(ns, attrName)
+        }
+      }
+      case false => {
+        Right(root.attribute(name))
+      }
+    }
+
+    searchRes.flatMap(convResult)
+  }
 }
 
 /**
