fe0e2a2c3ce3f5823a2c6a5132a31d86878d4434
Thomas Jakway <tjakway@nyu.edu>
2018-03-18 15:05:44 -0700

    wrote hasSubNodes

--- src/main/scala/com/jakway/gnucash/Accregex.scala
+++ src/main/scala/com/jakway/gnucash/Accregex.scala
@@ -1,7 +1,7 @@
 package com.jakway.gnucash
 
 
-import scala.xml.{Node, XML}
+import scala.xml.{Node, NodeSeq, XML}
 
 object Accregex {
   def main(args: Array[String]): Unit = {
@@ -28,13 +28,12 @@ object MatchTrans
 trait ValidationError
 
 /**
-  * TODO:
-  * -check count-data attribute cd:type=="book" && text=="1"
+  * boolean functions to run on XML nodes
   *
-  * -
   */
-object Validate {
-  private def onlyOne[A](errorType: String => ValidationError)(s: Seq[A]): Either[ValidationError, A] = {
+object NodeTests {
+
+  private def onlyOne[A](s: Seq[A])(implicit errorType: String => ValidationError): Either[ValidationError, A] = {
     val msg = "expected only 1 item in seq but got: "
     if(s.length == 1) {
       Right(s.head)
@@ -46,9 +45,36 @@ object Validate {
     }
   }
 
+  private def hasSubNodes(root: Node, name: String)
+                         (implicit errorType: String => ValidationError): Either[ValidationError, NodeSeq] = {
+
+    val sub = (root \ name)
+    sub.isEmpty match {
+      case true => Left(errorType(s"could not find subnode of $root named $name"))
+      case false => Right(sub)
+    }
+  }
+}
+
+/**
+  * TODO:
+  * -check count-data attribute cd:type=="book" && text=="1"
+  *
+  * -
+  */
+object Validate {
+
   private def isBook(node: Node): Boolean = {
     node.namespace == "gnc" && node.label == "book"
   }
+
+
+  def extractNumAccounts(book: Node): Either[ValidationError, Int] = {
+    case class ExtractNumAccountsError(msg: String) extends ValidationError
+
+    ???
+  }
+
   def apply(root: Node) = {
 
   }
