9f4f8899df4f5e339f0f2d3daae401600205f54e
Thomas Jakway <tjakway@nyu.edu>
2018-03-18 14:21:33 -0700

    2 basic xml tests

--- src/test/scala/com/jakway/gnucash/test/TestXMLUtils.scala
+++ src/test/scala/com/jakway/gnucash/test/TestXMLUtils.scala
@@ -1,9 +1,20 @@
 package com.jakway.gnucash.test
 
+import com.jakway.util.XMLUtils
 import org.scalatest._
 
-class TestXMLUtils extends FlatSpec with Matchers {
-  "XMLUtils.searchNode" should "find the node when there's only 1 element" in {
+import scala.xml.Elem
 
+class TestXMLUtils extends FlatSpec with Matchers {
+
+  val testElem: Elem = <testelem></testelem>
+
+  "XMLUtils.searchNode" should "find the node when there's only 1 element" in {
+    XMLUtils.searchNode(_ == testElem)(testElem) shouldEqual Seq(testElem)
+  }
+
+  //inverse of the above test
+  "XMLUtils.searchNode" should "not find the node when passed !=" in {
+    XMLUtils.searchNode(_ != testElem)(testElem) shouldEqual Seq()
   }
 }
