e00ca3dd96e461dd3d861b57bc24b6e72b8ad96b
Thomas Jakway <tjakway@nyu.edu>
2018-03-19 20:02:09 -0700

    wrote extractNumAccounts

--- src/main/scala/com/jakway/gnucash/validate/Validate.scala
+++ src/main/scala/com/jakway/gnucash/validate/Validate.scala
@@ -1,5 +1,8 @@
 package com.jakway.gnucash.validate
 
+import com.jakway.util.XMLUtils
+
+import scala.util.Try
 import scala.xml.Node
 
 /**
@@ -22,12 +25,22 @@ object Validate {
       findOnlyOne((isBookNode _, root))
     }
 
-  def isCountDataNode =
+  def isCountDataNode(n: Node): Boolean = {
+    //check if it's a number
+    Try(n.text.toInt).isSuccess &&
+      //and has the expected attribute and namespace
+      getAttribute((n, "cd:type")).isRight &&
+      hasNamespace((n, "gnc")).isRight
+  }
 
   def extractNumAccounts(book: Node): Either[ValidationError, Int] = {
     case class ExtractNumAccountsError(msg: String) extends ValidationError
     implicit def errorType: String => ValidationError = ExtractNumAccountsError.apply
 
+
+    onlyOne(XMLUtils.searchNode(isCountDataNode)(book)
+      .filter(n => getAttribute((n, "cd:type"))
+        .filterOrElse(_ == "account", Left()).isRight))
   }
 
   def apply(root: Node) = {
