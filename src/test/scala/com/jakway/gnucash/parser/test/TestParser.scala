8c2c950b5869a496eccc0785de8d374103b52fa4
Thomas Jakway <tjakway@nyu.edu>
2018-03-23 08:27:53 -0700

    tests compile

--- src/test/scala/com/jakway/gnucash/parser/test/TestParser.scala
+++ src/test/scala/com/jakway/gnucash/parser/test/TestParser.scala
@@ -1,8 +1,9 @@
 package com.jakway.gnucash.parser.test
 
 import com.jakway.gnucash.parser.{Parser, ValidationError}
+import org.scalatest.{FlatSpec, Matchers}
 
-import scala.xml.XML
+import scala.xml.{Node, XML}
 
 class TestParser(val regDocResource: String) extends FlatSpec with Matchers {
   val parser = new Parser
@@ -16,7 +17,9 @@ class TestParser(val regDocResource: String) extends FlatSpec with Matchers {
   val regDocRoot = XML.load(getClass.getResource(regDocResource))
 
 
-  val book = parser.findBookNode(regDocRoot)(TestParserLoadError).right.get
+  val book =
+    parser.findBookNode(regDocRoot)(TestParserLoadError.apply _).right.get
+
 
   "The Parser" should "load the number of accounts" in {
     parser.extractNumAccounts(book) shouldEqual Right(65)
