289cf41af81fa234b51fbfb5da56618c898eeea2
Thomas Jakway <tjakway@nyu.edu>
2018-03-26 15:38:03 -0400

    added misc (non-XML) ValidateF functions

--- src/main/scala/com/jakway/gnucash/parser/ValidateF.scala
+++ src/main/scala/com/jakway/gnucash/parser/ValidateF.scala
@@ -1,5 +1,6 @@
 package com.jakway.gnucash.parser
 
+import scala.util.{Failure, Success, Try}
 import scala.xml.Node
 
 //package private validation function class
@@ -23,5 +24,40 @@ object ValidateF {
   def getOrThrow[A](e: Either[ValidationError, A]): A = e match {
     case Right(a) => a
     case Left(e) => throw e
+  }
+
+  object MiscTests {
+    /**
+      * check that the passed seq is nonempty
+      * @tparam A
+      * @return
+      */
+    def notEmpty[A]: ValidateF[Seq[A], Seq[A]] =
+      (s: Seq[A], errorType: String => ValidationError) => s match {
+        case Seq() => Left(errorType(s"Expected $s to be nonempty"))
+        case _ => Right(s)
+      }
+
+
+    /**
+      * tests whether the trimmed string is nonempty
+      * @return
+      */
+    def nonEmptyStr: ValidateF[String, String] =
+      (s: String, errorType: String => ValidationError) => {
+        if(s.trim.isEmpty) {
+          Left(errorType(s"Expected $s to be nonempty"))
+        } else {
+          Right(s)
+        }
+      }
+
+    def isNumeric: ValidateF[String, Double] =
+      (str: String, errorType: String => ValidationError) => {
+        Try(str.toDouble) match {
+          case Success(d) => Right(d)
+          case Failure(_) => Left(errorType(s"Cannot parse '$str' as a number"))
+        }
+    }
   }
 }
