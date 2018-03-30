c237423f9a098c50e4e8fdb60cc9fdf57ed4de0d
Thomas Jakway <tjakway@nyu.edu>
2018-03-30 09:47:45 -0700

    wrote UniquePriorityOrdering

---
+++ src/main/scala/com/jakway/gnucash/rules/Priority.scala
@@ -0,0 +1,84 @@
+package com.jakway.gnucash.rules
+
+import com.jakway.gnucash.parser.ValidationError
+
+abstract class UniquePriorityOrdering[A, B](val toOrder: Seq[A]) {
+  class UniquePriorityOrderingError(override val msg: String)
+    extends ValidationError(msg)
+
+  case class EmptyArgument(override val msg: String)
+    extends UniquePriorityOrderingError(msg)
+
+
+  case class PriorityCollision(override val msg: String)
+    extends UniquePriorityOrderingError(msg)
+
+  implicit val ordering: Ordering[B]
+
+  /**
+    * extract the priority from the parameter
+    * @param obj
+    * @return
+    */
+  def getPriority(obj: A): B
+
+  /**
+    * @return the item with the highest priority
+    *         Error if >1 item has the highest priority
+    */
+  def getHighestPriority(): Either[ValidationError, A] = {
+    import scala.util.Sorting
+
+    if(toOrder.length < 0) {
+      Left(EmptyArgument("Expected argument with length >= 1 but got empty Seq"))
+    } else {
+      val sorted = Sorting.stableSort(toOrder, getPriority _)
+
+      assert(sorted.length == toOrder.length)
+
+      if(sorted.length == 1) {
+        Right(sorted.head)
+      } else {
+        val highestPriority = getPriority(sorted.head)
+
+        var count = 0
+        sorted.foreach { x =>
+          if(getPriority(x) == highestPriority) {
+            count += 1
+          }
+        }
+
+        if(count > 1) {
+          Left(PriorityCollision(s"More than 1 item has the highest priority in $sorted (expected exactly 1)"))
+        } else {
+          Right(sorted.head)
+        }
+      }
+    }
+  }
+}
+
+/**
+  * Lowest number = highest priority
+  */
+abstract class ZeroHighPriority[A](override val toOrder: Seq[A])
+  extends UniquePriorityOrdering[A, Int](toOrder) {
+
+  override implicit val ordering: Ordering[Int] = new Ordering[Int] {
+    //the opposite of what you would normally think
+    //since 0 priority = highest
+    override def compare(x: Int, y: Int): Int = {
+      val xHigher = -1
+      val xLower = 1
+      val equal = 0
+
+      if(x < y) {
+        xHigher
+      } else if(x > y) {
+        xLower
+      } else {
+        equal
+      }
+    }
+  }
+}
--- src/main/scala/com/jakway/gnucash/rules/TransactionRule.scala
+++ src/main/scala/com/jakway/gnucash/rules/TransactionRule.scala
@@ -1,14 +1,51 @@
 package com.jakway.gnucash.rules
 
-import com.jakway.gnucash.parser.LinkedAccount
+import java.util.Comparator
+
+import com.jakway.gnucash.parser.{LinkedAccount, ValidationError}
 
 import scala.util.matching.Regex
 
 
-
+/**
+  *
+  * @param pattern
+  * @param priority
+  * @param sourceAccount
+  * @param destAccount the account we're transferring to if the rule matches
+  */
 case class LinkedTransactionRule(pattern: Regex,
                                  priority: Double,
                                  sourceAccount: LinkedAccount,
                                  destAccount: LinkedAccount)
 
+
+/**
+  * contains transaction matching and transformation logic
+  * @param destAccount the account to move transactions from (probably "Unspecified")
+  */
+class RuleApplicator(val destAccount: LinkedAccount, val rules: Set[LinkedTransactionRule]) {
+  import RuleApplicator._
+
+  class RuleApplicatorError(override val msg: String)
+    extends ValidationError(msg)
+
+  def whichRule(transactionInput: TransactionInput): Either[ValidationError, LinkedTransactionRule] = {
+
+  }
+
+  def ruleMatches(i: TransactionInput)(rule: LinkedTransactionRule): Boolean = {
+    i.destAccount == destAccount &&
+    i.sourceAccount == rule.sourceAccount &&
+    rule.pattern.findFirstMatchIn(i.description).isDefined
+  }
+
+}
+
+object RuleApplicator {
+  case class TransactionInput(description: String,
+                              sourceAccount: LinkedAccount,
+                              destAccount: LinkedAccount)
+
+}
 
