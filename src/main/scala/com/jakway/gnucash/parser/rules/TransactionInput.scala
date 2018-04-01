f7ee1235b6dd1caa7a90ab8248eb79764db613aa
Thomas Jakway <tjakway@nyu.edu>
2018-03-31 17:15:13 -0700

    wrote isSourceAccount and isDestAccount using HOFs

--- src/main/scala/com/jakway/gnucash/parser/rules/TransactionInput.scala
+++ src/main/scala/com/jakway/gnucash/parser/rules/TransactionInput.scala
@@ -6,30 +6,52 @@ case class Transaction(id: String,
                        description: String,
                        splits: Seq[Split]) {
 
-  /**
-    * is the passed account a source of funds
-    * aka whether the transaction debits from the linked account
-    * @param linkedAccount
-    * @return
-    */
-  def isSourceAccount(linkedAccount: LinkedAccount): Boolean = {
+  private def cmpAccount(cmp: Double => Boolean)(linkedAccount: LinkedAccount): Boolean = {
     splits.foldLeft(false) {
       case (true, _) => true
       case (false, Split(_, account, value))
         if(account == linkedAccount) =>  {
 
-        //check whether we deduct funds from that account
-        if(value < 0) {
-          true
-        } else {
-          false
-        }
+        //check the sign of value
+        cmp(value)
       }
 
         //ignore other accounts
       case (acc, _) => acc
     }
   }
+
+  /**
+    * is the passed account a source of funds
+    * aka whether the transaction debits from the linked account
+    * @return
+    */
+  def isSourceAccount: LinkedAccount => Boolean = cmpAccount(isDebitor)
+
+
+  /**
+    * is the passed account given funds by this transaction
+    * aka whether the transaction credits to the linked account
+    * @return
+    */
+  def isDestAccount: LinkedAccount => Boolean = cmpAccount(isCreditor)
+
+  def isDebitor: Double => Boolean =
+    value =>
+      if(value < 0) {
+        true
+      } else {
+        false
+      }
+
+  //TODO: not sure if 0 should be a creditor or debtor
+  def isCreditor: Double => Boolean =
+    value =>
+      if(value >= 0) {
+        true
+      } else {
+        false
+      }
 
   /**
     * for a transaction to be valid all splits must sum to 0
