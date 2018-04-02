a78b98392402e280f9a710a0c76a048444a2ee3f
Thomas Jakway <tjakway@nyu.edu>
2018-04-02 10:53:10 -0700

    fixup! wrote NodeReplace and TestNodeReplace

--- src/test/scala/com/jakway/gnucash/test/TestNodeReplace.scala
+++ src/test/scala/com/jakway/gnucash/test/TestNodeReplace.scala
@@ -6,26 +6,53 @@ import org.scalatest.{FlatSpec, Matchers}
 
 import scala.xml.{Elem, Node}
 
+/**
+  * transparently keep count of the number of times predicateElem returned true
+  * @tparam A
+  */
+abstract class ElemReplaceCountPredicate[A] extends ElemReplace[A] {
+  var countPredicateTrue = 0
+
+  def predicateEC(e: Elem): Boolean
+
+  /**
+    * can't call super.predicateElem unless we're a trait...
+    * @param e
+    * @return
+    */
+  final override def predicateElem(e: Elem): Boolean = {
+    if(predicateEC(e)) {
+      countPredicateTrue = countPredicateTrue + 1
+      true
+    } else {
+      false
+    }
+  }
+}
+
 class TestNodeReplace extends FlatSpec with Matchers {
   import com.jakway.gnucash.parser.xml.NodeTests._
 
   "NodeReplace" should "transform subelements" in {
     val newLabel = "book_trans"
 
-    implicit def errorType: String => ValidationError = {
-      throw new RuntimeException("getAttribute shouldn't fail in TestNodeReplace")
-      ???
-    }
+    case class TestNodeReplaceError(override val msg: String)
+      extends ValidationError(msg)
 
-    class BookReplace extends ElemReplace[String] {
-      override def predicateElem(e: Elem): Boolean = e.label == "book"
+    implicit def errorType: String => ValidationError = TestNodeReplaceError.apply
+
+    class BookReplace extends ElemReplaceCountPredicate[String] {
+      override def predicateEC(e: Elem): Boolean = e.label == "book"
 
       override def replaceElem(e: Elem): (String, Node) = {
         (getAttribute((e, "id")).right.get, e.copy(label = newLabel))
       }
     }
 
-    val (tags, newBookTree) = new BookReplace().apply(BooksLiteral.books)
+    val replaceF = new BookReplace()
+    val (tags, newBookTree) = replaceF.apply(BooksLiteral.books)
+
+    replaceF.countPredicateTrue shouldEqual 2
 
     tags.length shouldEqual BooksLiteral.numBooks
 
