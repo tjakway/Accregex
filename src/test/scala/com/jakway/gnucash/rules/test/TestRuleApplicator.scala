92e4359cd8650130a9aabea9c2533d3e8c07419d
Thomas Jakway <tjakway@nyu.edu>
2018-04-02 18:04:30 -0700

    fixup! began writing integration test

--- src/test/scala/com/jakway/gnucash/rules/test/TestRuleApplicator.scala
+++ src/test/scala/com/jakway/gnucash/rules/test/TestRuleApplicator.scala
@@ -1,14 +1,19 @@
 package com.jakway.gnucash.rules.test
 
 import com.jakway.gnucash.parser.rules.UnlinkedTransactionRule
-import com.jakway.gnucash.parser.{AccountNameParser, LinkedAccount, Parser}
-import com.jakway.gnucash.rules.LinkedTransactionRule
+import com.jakway.gnucash.parser.xml.NodeTests
+import com.jakway.gnucash.parser.{AccountNameParser, LinkedAccount, Parser, ValidationError}
+import com.jakway.gnucash.rules.{LinkedTransactionRule, RuleApplicator}
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.xml.Node
 
 class TestRuleApplicator(val regDocRoot: Node) extends FlatSpec with Matchers {
   val parser: Parser = new Parser()
+
+  case class TestRuleApplicatorError(override val msg: String)
+    extends ValidationError(msg)
+  implicit def errorType: String => ValidationError = TestRuleApplicatorError.apply
 
   "RuleApplicator" should "change gas to charity" in {
     val allAccounts: Map[String, LinkedAccount] = Parser.linkAccounts(parser
@@ -20,15 +25,33 @@ class TestRuleApplicator(val regDocRoot: Node) extends FlatSpec with Matchers {
       .map(l => (l.id, l))
       .toMap
 
+    val gasAccountString = "Expense:Auto:Gas"
     val unlinkedChangeGasRule = UnlinkedTransactionRule(".*",
-      "1", "Expense:Auto:Gas", "Expense:Charity")
+      "1", "Assets:Current Assets", "Expense:Charity")
 
-    //link the changeGasRule to the referenced account names
     val nameParser = new AccountNameParser(allAccounts.values.toSeq)
-    val changeGasRule = UnlinkedTransactionRule
-      .link(nameParser)(unlinkedChangeGasRule)
 
-    
+    val applicatorE = for {
+      //link the changeGasRule to the referenced account names
+      changeGasRule <- UnlinkedTransactionRule
+                      .link(nameParser)(unlinkedChangeGasRule)
+
+      gasAccount <- nameParser.findReferencedAccount(gasAccountString)
+    } yield {
+      new RuleApplicator(
+        allAccounts, gasAccount, Set(changeGasRule))
+    }
+
+    applicatorE.isRight shouldEqual true
+    val applicator = applicatorE.right.get
+
+    val allTransactionNodes = NodeTests.getElems((regDocRoot, "transaction"))
+      .right.get
+
+    val newNodes = allTransactionNodes.map(applicator.apply(_)._2)
+
+    newNodes == allTransactionNodes shouldEqual false
+
   }
 
 }
