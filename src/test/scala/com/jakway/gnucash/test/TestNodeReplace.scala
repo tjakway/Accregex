9bd1595f462beaa33cfc19a131887d142807d040
Thomas Jakway <tjakway@nyu.edu>
2018-04-04 17:58:29 -0700

    wrote tests of NodeReplace without a replacement function

--- src/test/scala/com/jakway/gnucash/test/TestNodeReplace.scala
+++ src/test/scala/com/jakway/gnucash/test/TestNodeReplace.scala
@@ -6,7 +6,7 @@ import com.jakway.gnucash.test.objects.BooksLiteral
 import com.jakway.util.XMLUtils
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala.xml.{Elem, Node}
+import scala.xml.{Elem, Node, SpecialNode}
 
 /**
   * transparently keep count of the number of times predicateElem returned true
@@ -35,13 +35,13 @@ abstract class ElemReplaceCountPredicate[A] extends ElemReplace[A] {
 class TestNodeReplace extends FlatSpec with Matchers {
   import com.jakway.gnucash.parser.xml.NodeTests._
 
+  case class TestNodeReplaceError(override val msg: String)
+    extends ValidationError(msg)
+
+  implicit def errorType: String => ValidationError = TestNodeReplaceError.apply
+
   "NodeReplace" should "transform subelements" in {
     val newLabel = "book_trans"
-
-    case class TestNodeReplaceError(override val msg: String)
-      extends ValidationError(msg)
-
-    implicit def errorType: String => ValidationError = TestNodeReplaceError.apply
 
     class BookReplace extends ElemReplaceCountPredicate[String] {
       override def predicateEC(e: Elem): Boolean = e.label == "book"
@@ -74,5 +74,45 @@ class TestNodeReplace extends FlatSpec with Matchers {
     //and the children of the changed tree should have the new label
     XMLUtils.filterPCData(newBookTree.child)
       .map(_.label).toSet shouldEqual Set(newLabel)
+  }
+
+  /**
+    * if replace is just the identity function then the tree
+    * should stay the same
+    */
+  def testNoReplaceVisitChildren(tree: Node) = {
+    abstract class IdNodeReplace extends ElemReplaceCountPredicate[String] {
+      override protected def replaceElem(e: Elem): (String, Node) = (e.label, e)
+    }
+    class VisitOnlyChildren extends IdNodeReplace {
+      override def predicateEC(e: Elem): Boolean =
+      //text doesn't count as a child node
+        if(e.child.filter(!_.isInstanceOf[SpecialNode]).isEmpty) {
+          true
+        } else {
+          false
+        }
+      }
+
+    val replaceF = new VisitOnlyChildren()
+    val (labels, outputNodes) = replaceF.doReplace(tree)
+    labels.length > 0 shouldEqual true
+    outputNodes.isEmpty shouldEqual false
+
+    tree.xml_==(outputNodes) shouldEqual true
+
+    (labels.map(_._1), outputNodes)
+  }
+
+
+  it should "not change the structure of the book tree" in {
+    val (labels, outputNodes) = testNoReplaceVisitChildren(BooksLiteral.books)
+
+    labels.toSet shouldEqual Set("book")
+    labels.length shouldEqual 2
+  }
+
+  it should "not change the structure of the reg doc tree" in {
+
   }
 }
