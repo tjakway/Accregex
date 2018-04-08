e39d88f2d18cf20b954baf72207ef329894449c3
Thomas Jakway <tjakway@nyu.edu>
2018-04-08 10:34:52 -0700

    added skeleton ErrorPrinter

--- src/main/scala/com/jakway/gnucash/io/Driver.scala
+++ src/main/scala/com/jakway/gnucash/io/Driver.scala
@@ -1,7 +1,16 @@
 package com.jakway.gnucash.io
 
 import com.jakway.gnucash.ValidatedConfig
+import com.jakway.gnucash.parser.{Parser, ValidationError}
 
 class Driver(val config: ValidatedConfig) {
+  val parser = new Parser()
+
   def run(): Unit = ???
+
+  private def loadAccounts = {
+
+  }
+
+  private def loadGnucashXMLFile(): Either[ValidationError, ]
 }
---
+++ src/main/scala/com/jakway/gnucash/io/ErrorPrinter.scala
@@ -0,0 +1,8 @@
+package com.jakway.gnucash.io
+
+import com.jakway.gnucash.parser.ValidationError
+
+object ErrorPrinter {
+  //TODO
+  def format(validationError: ValidationError): String = ???
+}
