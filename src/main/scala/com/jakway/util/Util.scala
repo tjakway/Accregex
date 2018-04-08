7a61301668e77c7c0198ae74b998a6669b2f52e9
Thomas Jakway <tjakway@nyu.edu>
2018-04-07 22:21:07 -0700

    wrote reEither and either util functions

--- src/main/scala/com/jakway/util/Util.scala
+++ src/main/scala/com/jakway/util/Util.scala
@@ -4,4 +4,14 @@ object Util {
   def anyOf[A](s: TraversableOnce[A])(f: A => Boolean): Boolean = s.foldLeft(false) {
     case (b, x) => b || f(x)
   }
+
+  def reEither[A, B, C, D](a: Either[A, B])(fLeft: A => C)(fRight: B => D): Either[C, D] = a match {
+    case Right(x) => Right(fRight(x))
+    case Left(y) => Left(fLeft(y))
+  }
+
+  def either[A, B, C](a: Either[A, B])(ifLeft: A => C)(ifRight: B => C): C = a match {
+    case Right(x) => ifRight(x)
+    case Left(y) => ifLeft(y)
+  }
 }
