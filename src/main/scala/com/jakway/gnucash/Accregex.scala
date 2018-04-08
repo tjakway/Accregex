dbcc15af172a6a1602248855f580f8064183ed98
Thomas Jakway <tjakway@nyu.edu>
2018-04-08 10:28:41 -0700

    wrote main method and Driver class skeleton

--- src/main/scala/com/jakway/gnucash/Accregex.scala
+++ src/main/scala/com/jakway/gnucash/Accregex.scala
@@ -1,7 +1,24 @@
 package com.jakway.gnucash
 
+import com.jakway.gnucash.io.Driver
+
 
 object Accregex {
+  def fatalError(msg: String): Unit = {
+    System.err.println(s"Fatal error in ${Config.progName}: $msg")
+    System.exit(1)
+  }
+
   def main(args: Array[String]): Unit = {
+    UnvalidatedConfig
+      .parser.parse(args, UnvalidatedConfig.default)
+      .map { unvalidatedConfig =>
+        unvalidatedConfig.validate() match {
+          case Right(validatedConfig) =>
+            new Driver(validatedConfig).run()
+
+          case Left(errMsg) => fatalError(errMsg)
+        }
+      }
   }
 }
---
+++ src/main/scala/com/jakway/gnucash/io/Driver.scala
@@ -0,0 +1,7 @@
+package com.jakway.gnucash.io
+
+import com.jakway.gnucash.ValidatedConfig
+
+class Driver(val config: ValidatedConfig) {
+  def run(): Unit = ???
+}
