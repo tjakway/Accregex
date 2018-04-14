fc73b595c2819f150bc3ae61391bacadd39fcccd
Thomas Jakway <tjakway@nyu.edu>
2018-04-14 12:13:04 -0700

    adapted tests for XMLLintValidator

--- src/test/scala/com/jakway/gnucash/io/test/TestXMLValidator.scala
+++ src/test/scala/com/jakway/gnucash/io/test/TestXMLValidator.scala
@@ -3,7 +3,7 @@ package com.jakway.gnucash.io.test
 import java.io.{File, PrintWriter}
 import java.nio.file.Files
 
-import com.jakway.gnucash.io.GnucashXMLValidator
+import com.jakway.gnucash.io.XMLLintValidator
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.xml.{Elem, Node, XML}
@@ -14,8 +14,8 @@ class TestXMLValidator(val regDocRoot: Node) extends FlatSpec with Matchers {
   val regDocElem = regDocRoot.asInstanceOf[Elem]
 
   "XMLValidator" should "support >1 validator instance simultaneously" in {
-    val v1 = new GnucashXMLValidator()
-    val v2 = new GnucashXMLValidator()
+    val v1 = new XMLLintValidator()
+    val v2 = new XMLLintValidator()
   }
 
   def toTempFile(n: Node): File = {
@@ -30,16 +30,16 @@ class TestXMLValidator(val regDocRoot: Node) extends FlatSpec with Matchers {
   }
 
   "XMLValidator" should "validate reg_doc_example as a Node" in {
-    new GnucashXMLValidator().validate(inputName, regDocRoot) shouldEqual Right()
+    new XMLLintValidator().validateNode(inputName, regDocRoot) shouldEqual Right(())
   }
 
   "XMLValidator" should "validate reg_doc_example as a file" in {
     val tf = toTempFile(regDocElem.copy())
-    val res = new GnucashXMLValidator().validate(tf)
+    val res = new XMLLintValidator().validate(tf)
 
     tf.delete()
 
-    res shouldEqual Right()
+    res shouldEqual Right(())
   }
 
 }
