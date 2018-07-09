426c4f71e8b455db8b74ec4be170c1d85fe3a353
Thomas Jakway <tjakway@nyu.edu>
2018-07-09 08:44:20 -0700

    wrote checkAndDeleteFile

--- src/test/scala/com/jakway/gnucash/io/test/TestOutput.scala
+++ src/test/scala/com/jakway/gnucash/io/test/TestOutput.scala
@@ -2,45 +2,57 @@ package com.jakway.gnucash.io.test
 
 import java.io.File
 
-import com.jakway.gnucash.error.ValidationError
+import com.jakway.gnucash.error.{MultiValidationError, ValidationError}
 import com.jakway.gnucash.test.IntegrationTests.HasFoodTestConf
 import com.jakway.gnucash.test.ResourceFiles
+import com.jakway.util.Util
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 object TestOutput {
   case class TestOutputException(override val msg: String)
     extends ValidationError(msg)
 
-  def checkAndDeleteFile(checks: Seq[File => Option[String]])(f: => File): Unit = {
-    val ex: Option[ValidationError] = {
-      //should be a regular file
-      if(!f.exists()) {
-        Some(s"$f does not exist")
-      } else if(!f.canRead) {
-        Some(s"Cannot read file $f")
-      } else if(f.isDirectory) {
-        Some(s"$f is a directory")
-      } else {
-        None
-      }
-    }.map(TestOutputException.apply)
+  def checkAndDeleteFile(checks: Seq[File => Either[String, Unit]])(f: => File): Unit = {
+
+    val readableFileChecks: Seq[File => Either[String, Unit]] = {
+      def fExists(f: File) =
+        if(!f.exists) {
+          Left(s"$f does not exist")
+        } else Right(())
+
+      def canRead(f: File) =
+        if(!f.canRead) {
+          Left(s"Cannot read file $f")
+        } else {
+          Right(())
+        }
+
+      def isNotDirectory(f: File) =
+        if(f.isDirectory) {
+          Left(s"$f is a directory")
+        } else {
+          Right(())
+        }
+
+      Seq(fExists, canRead, isNotDirectory)
+    }
+
+    val allChecks = checks ++ readableFileChecks
+
+    val checkResults = Util.checkAll(TestOutputException.apply)(allChecks)(f)
 
     //delete the file
     //if we can't delete it combine that error with any previous errors
-    val deleteRes =
-      if(!f.delete()) {
-        val newEx = TestOutputException(s"Could not delete $f")
+    if(!f.delete()) {
+      val couldNotDeleteMsg = s"Could not delete $f"
 
-        Some(ex.map(oldEx => TestOutputException(s"2 errors occurred: `${oldEx.msg}` then " +
-          s"`${newEx.msg}`")).getOrElse(newEx))
-      } else {
-        None
+      checkResults match {
+        case Left(errs) => TestOutputException(s"At least 2 errors occurred: `${errs.msg}` then " +
+          s"`$couldNotDeleteMsg`")
+        case Right(()) => TestOutputException(couldNotDeleteMsg)
       }
-
-    //throw any exceptions we found
-    deleteRes match {
-      case Some(ex) => throw ex
-      case None => Unit
+    } else {
+      checkResults
     }
   }
 }
